478e3c8f39a945e022aac1e3acc34fb40db0bd3b
Johan Tjelldén <johan@hedvig.com>
2017-10-23 10:51:33 +0200

    Nicly formatting of null Entrances

--- member-service/src/main/java/com/hedvig/memberservice/web/dto/Member.java
+++ member-service/src/main/java/com/hedvig/memberservice/web/dto/Member.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Value;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 @Value
 @AllArgsConstructor
@@ -31,7 +32,7 @@ public class Member {
         this.firstName = memberEntity.getFirstName();
         this.lastName = memberEntity.getLastName();
         this.memberId = memberEntity.getId();
-        this.street = String.format("%s %s%s", memberEntity.getStreetName(), memberEntity.getStreetNumber(), memberEntity.getEntrance());
+        this.street = String.format("%s %s%s", memberEntity.getStreetName(), memberEntity.getStreetNumber(), Objects.toString(memberEntity.getEntrance()));
         this.zipCode = memberEntity.getPostalCode();
         this.city = memberEntity.getCity();
         this.email = "";
