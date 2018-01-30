cf3d1297c2cec77a467e71a8f3bb5b62f38b9078
Johan Tjelldén <johan@hedvig.com>
2018-01-30 14:39:59 +0100

    New cashbacks

--- member-service/src/main/java/com/hedvig/memberservice/services/CashbackService.java
+++ member-service/src/main/java/com/hedvig/memberservice/services/CashbackService.java
@@ -12,44 +12,31 @@ public class CashbackService {
 
     public CashbackService() {
 
-        CashbackOption option1 = new CashbackOption(
-                UUID.fromString("97b2d1d8-af4a-11e7-9b2b-bbc138162bb2"),
-                "Rädda barnen",
-                "Rädda Barnen kämpar för barns rättigheter i Sverige och i världen. Vi arbetar på lång- och kort sikt för att inget barn ska behöva fara illa.",
-                false,
-                true,
-                "https://www.rawfoodmiddagar.com/images/webshopen/rc3a4dda-barnen-logga-sv.jpg",
-                "https://www.rawfoodmiddagar.com/images/webshopen/rc3a4dda-barnen-logga-sv.jpg",
-                "Elisabeth Dahlin, Generaldirektör",
-                "\"Tack kära %s för att du bidrar till att hjälpa fler utsatta barn\""
-        );
-
         CashbackOption option3 = new CashbackOption(
-                UUID.fromString("d24c427e-d110-11e7-a47e-0b4e39412e98"),
-                "Bancancerfonden",
-                "Barncancerfonden arbetar för att bekämpa barncancer och se till att drabbade och deras familjer får den vård och stöd de behöver.",
-                false,
+                UUID.fromString("97b2d1d8-af4a-11e7-9b2b-bbc138162bb2"),
+                "SOS Barnbyar",
+                "Ge utsatta barn en trygg uppväxt.",
                 true,
-                "http://ljus.barncancerfonden.se/images/barncancerfonden.png",
-                "http://ljus.barncancerfonden.se/images/barncancerfonden.png",
-                "Isabelle Ducellier, Generaldirektör",
-                "\"Tack kära %s för att du bidrar till att rädda livet på fler cancerdrabbade barn\""
+                true,
+                "https://s3.eu-central-1.amazonaws.com/com-hedvig-web-content/sos.png",
+                "https://s3.eu-central-1.amazonaws.com/com-hedvig-web-content/sos.png",
+                "Lucas Carlsén, VD Hedvig AB",
+                "\"Tack %s! Ditt stöd gör stor skillnad\""
         );
 
         CashbackOption option2 = new CashbackOption(
                 UUID.fromString("11143ee0-af4b-11e7-a359-4f8b8d55e69f"),
-                "Stadsmissionen",
-                "Stockholms Stadsmission är en idéburen organisation som arbetar för att skapa ett mänskligare samhälle för alla.",
+                "Cancerfonden",
+                "Hjälp till att besegra cancer.",
                 false,
                 true,
-                "https://www.stadsmissionen.se/profiles/ssm/themes/custom/ssmtheme/images/share_default.png",
-                "https://www.stadsmissionen.se/profiles/ssm/themes/custom/ssmtheme/images/share_default.png",
-                "Marika Markovits, Direktör",
-                "\"Tack %s! Ditt stöd gör stor skillnad\""
+                "https://s3.eu-central-1.amazonaws.com/com-hedvig-web-content/cancerfonden.png",
+                "https://s3.eu-central-1.amazonaws.com/com-hedvig-web-content/cancerfonden.png",
+                "John Ardelius, CTO Hedvig AB",
+                "\"Tack %s! Jag tycker att Cancerfonden gör ett bra arbete!\""
         );
 
         options.put(option3.id, option3);
-        options.put(option1.id, option1);
         options.put(option2.id, option2);
     }
 
@@ -62,7 +49,7 @@ public class CashbackService {
     }
 
     public UUID getDefaultId() {
-        return UUID.fromString("d24c427e-d110-11e7-a47e-0b4e39412e98");
+        return UUID.fromString("97b2d1d8-af4a-11e7-9b2b-bbc138162bb2");
     }
 
     public CashbackOption getDefaultCashback() {
