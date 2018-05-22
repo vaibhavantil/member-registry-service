4306138ece9c40e99bb43f3ced56678c1acae511
Johan Tjelldén <johan@hedvig.com>
2018-05-22 15:14:25 +0200

    TrackingIdEntity does not have ssn field

--- member-service/src/main/java/com/hedvig/memberservice/query/TrackingIdRepository.java
+++ member-service/src/main/java/com/hedvig/memberservice/query/TrackingIdRepository.java
@@ -9,6 +9,5 @@ import java.util.UUID;
 @Repository
 public interface TrackingIdRepository extends JpaRepository<TrackingIdEntity, Long> {
     Optional<TrackingIdEntity> findById(long s);
-    Optional<TrackingIdEntity> findBySsn(String s);
     Optional<TrackingIdEntity> findByTrackingId(UUID s);
 }
