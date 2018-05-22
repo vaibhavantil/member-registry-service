fc5f6d6a97cc0bc077f5c3f39616af8ddfdd5066
Johan Tjelldén <johan@hedvig.com>
2018-05-22 15:03:55 +0200

    Mark TrackingIdRepository with @Repository

--- member-service/src/main/java/com/hedvig/memberservice/query/TrackingIdRepository.java
+++ member-service/src/main/java/com/hedvig/memberservice/query/TrackingIdRepository.java
@@ -1,10 +1,12 @@
 package com.hedvig.memberservice.query;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface TrackingIdRepository extends JpaRepository<TrackingIdEntity, Long> {
     Optional<TrackingIdEntity> findById(long s);
     Optional<TrackingIdEntity> findBySsn(String s);
