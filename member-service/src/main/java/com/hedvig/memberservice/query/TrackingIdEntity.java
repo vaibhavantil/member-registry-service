55cee43a1726213855e0d654e6e25d638b501b4d
Johan Tjelldén <johan@hedvig.com>
2018-05-23 11:25:58 +0200

    Use memberId as key in TrackingIdEntity

--- member-service/src/main/java/com/hedvig/memberservice/query/TrackingIdEntity.java
+++ member-service/src/main/java/com/hedvig/memberservice/query/TrackingIdEntity.java
@@ -14,10 +14,6 @@ public class TrackingIdEntity {
     @Id
     @Getter
     @Setter
-    private Long id;
-
-    @Getter
-    @Setter
     @Column(unique = true)
     private String memberId;
     
