68876cdc4c741cccaa3dd1ce2b054b6a9e680641
meletios <meletios@hedvig.com>
2018-08-23 15:51:11 +0200

    delete firebase integration

--- notification-service/src/main/java/com/hedvig/memberservice/notificationService/serviceIntegration/firebase/FirebaseNotificationService.java
+++
@@ -1,7 +0,0 @@
-package com.hedvig.memberservice.notificationService.serviceIntegration.firebase;
-
-public interface FirebaseNotificationService {
-  void sendNewMessageNotification(String fcmToken);
-
-  void sendNotification(String memberId, String message);
-}
