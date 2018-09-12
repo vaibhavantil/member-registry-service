832228ef01657643e7e575a46d9ab4aec5d4c55f
Johan Tjelldén <johan@hedvig.com>
2018-09-12 13:56:12 +0200

    Fix for old MemberSignedEvents

--- member-service/src/main/java/com/hedvig/memberservice/events/MemberSignedEvent.java
+++ member-service/src/main/java/com/hedvig/memberservice/events/MemberSignedEvent.java
@@ -9,4 +9,12 @@ public class MemberSignedEvent {
   private final String referenceId;
   private final String signature;
   private final String oscpResponse;
+
+
+  /**
+   * @deprecated This field exists on some of the events in the database, these events should be cleand up.
+   * if you need to get the signedOnDate get the eventTimeStamp instead
+   */
+  @Deprecated()
+  private final String signedOn = null;
 }
