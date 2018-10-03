9344e3b8896fa0999be180393e9779277209bfca
meletios <meletios@hedvig.com>
2018-10-03 11:09:48 +0200

    add FeignConfiguration

---
+++ bank-id/src/main/java/com/hedvig/external/bankID/configuration/FeignConfiguration.java
@@ -0,0 +1,17 @@
+package com.hedvig.external.bankID.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.hedvig.external.bankID.bankIdRest.BankIdRestErrorDecoder;
+import feign.codec.ErrorDecoder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignConfiguration {
+
+  @Bean
+  ErrorDecoder errorDecoder(ObjectMapper objectMapper) {
+    return new BankIdRestErrorDecoder(objectMapper);
+  }
+
+}
