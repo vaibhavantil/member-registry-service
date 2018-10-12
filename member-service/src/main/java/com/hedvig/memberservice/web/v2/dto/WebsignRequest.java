66133a4a8099c217e91202dd6ceec249a6fb1e4f
meletios <meletios@hedvig.com>
2018-10-12 14:26:53 +0200

    validate ipAddress

--- member-service/src/main/java/com/hedvig/memberservice/web/v2/dto/WebsignRequest.java
+++ member-service/src/main/java/com/hedvig/memberservice/web/v2/dto/WebsignRequest.java
@@ -1,11 +1,16 @@
 package com.hedvig.memberservice.web.v2.dto;
 
+import com.google.common.net.InetAddresses;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import lombok.Value;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Value
 public class WebsignRequest {
+
+  private Logger logger = LoggerFactory.getLogger(WebsignRequest.class);
 
   @Email
   String email;
@@ -16,4 +21,15 @@ public class WebsignRequest {
   @NotBlank
   String ipAddress;
 
+  public WebsignRequest(@Email String email, @NotBlank String ssn, @NotBlank String ipAddress) {
+    this.email = email;
+    this.ssn = ssn;
+    this.ipAddress = GetValidIpAddress(ipAddress);
+  }
+
+  private String GetValidIpAddress(String ipAddress) {
+    boolean isValid = InetAddresses.isInetAddress(ipAddress);
+    logger.info("Validating ip {}, isValid {}", ipAddress, isValid);
+    return isValid ? ipAddress : "1.1.1.1";
+  }
 }
