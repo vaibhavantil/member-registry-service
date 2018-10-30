48b2b734009bddcd0c5301dd332371e757aa0dab
Oscar Nylander <oscar@hedvig.com>
2018-10-30 12:06:08 +0100

    Catch errors and log them

--- member-service/src/main/java/com/hedvig/memberservice/services/SNSNotificationService.java
+++ member-service/src/main/java/com/hedvig/memberservice/services/SNSNotificationService.java
@@ -3,7 +3,10 @@ package com.hedvig.memberservice.services;
 import org.springframework.cloud.aws.messaging.core.NotificationMessagingTemplate;
 import org.springframework.stereotype.Service;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class SNSNotificationService {
 
   private final NotificationMessagingTemplate template;
@@ -12,7 +15,11 @@ public class SNSNotificationService {
     this.template = notificationMessagingTemplate;
   }
 	public void sendMemberSignedNotification(Long id) {
-    template.sendNotification("newMembers", String.format("New member signed! Member has id %s", id), "CallMe");
+    try {
+      template.sendNotification("newMembers", String.format("New member signed! Member has id %s", id), "CallMe");
+    } catch (Exception e) {
+      log.error("Could not send notification to SNS: ", e);
+    }
 	}
 
 }
