ee7d7c98c49c7e0c318d17e8cc5e10e351109091
Oscar Nylander <oscar@hedvig.com>
2018-10-30 10:25:32 +0100

    WIP: Support adding tracking ID's to web-onboarding members

--- member-service/src/main/java/com/hedvig/memberservice/web/MembersController.java
+++ member-service/src/main/java/com/hedvig/memberservice/web/MembersController.java
@@ -1,5 +1,6 @@
 package com.hedvig.memberservice.web;
 
+import com.hedvig.memberservice.commands.AssignTrackingIdCommand;
 import com.hedvig.memberservice.commands.CreateMemberCommand;
 import com.hedvig.memberservice.externalApi.productsPricing.ProductApi;
 import com.hedvig.memberservice.externalApi.productsPricing.dto.InsuranceStatusDTO;
@@ -12,6 +13,8 @@ import com.hedvig.memberservice.web.dto.CashbackOption;
 import com.hedvig.memberservice.web.dto.CounterDTO;
 import com.hedvig.memberservice.web.dto.Member;
 import com.hedvig.memberservice.web.dto.Profile;
+import com.hedvig.memberservice.web.dto.TrackingIdDto;
+
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.time.LocalDate;
@@ -30,6 +33,7 @@ import org.springframework.retry.support.RetryTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -53,13 +57,8 @@ public class MembersController {
   public String counterKey;
 
   @Autowired
-  public MembersController(
-      MemberRepository repo,
-      CommandGateway commandGateway,
-      RetryTemplate retryTemplate,
-      ProductApi productApi,
-      CashbackService cashbackService,
-      TrackingIdRepository trackingRepo)
+  public MembersController(MemberRepository repo, CommandGateway commandGateway, RetryTemplate retryTemplate,
+      ProductApi productApi, CashbackService cashbackService, TrackingIdRepository trackingRepo)
       throws NoSuchAlgorithmException {
     this.repo = repo;
     this.commandGateway = commandGateway;
@@ -96,88 +95,73 @@ public class MembersController {
   @PostMapping("/helloHedvig")
   public ResponseEntity<String> helloHedvig() throws Exception {
 
-    Long id =
-        retryTemplate.execute(
-            arg -> {
-              Long memberId;
-              Optional<MemberEntity> member;
-              do {
-                memberId = Math.abs(this.randomGenerator.nextLong() % 1000000000);
-                member = repo.findById(memberId);
-              } while (member.isPresent());
+    Long id = retryTemplate.execute(arg -> {
+      Long memberId;
+      Optional<MemberEntity> member;
+      do {
+        memberId = Math.abs(this.randomGenerator.nextLong() % 1000000000);
+        member = repo.findById(memberId);
+      } while (member.isPresent());
 
-              CompletableFuture<Object> a = commandGateway.send(new CreateMemberCommand(memberId));
-              Object ret = a.get();
-              log.info(ret.toString());
-              return memberId;
-            });
+      CompletableFuture<Object> a = commandGateway.send(new CreateMemberCommand(memberId));
+      Object ret = a.get();
+      log.info(ret.toString());
+      return memberId;
+    });
 
     log.info("New member created with id: " + id);
-    return ResponseEntity.ok()
-        .contentType(MediaType.APPLICATION_JSON)
-        .body("{\"memberId\":" + id + "}");
+    return ResponseEntity.ok().contentType(MediaType.APPLICATION_JSON).body("{\"memberId\":" + id + "}");
+  }
+
+  @PostMapping("/trackingId")
+  public ResponseEntity<Void> setTrackingId(@RequestHeader(value = "hedvig.token", required = true) Long hid,
+      @RequestBody TrackingIdDto trackingIdDto) {
+    commandGateway.sendAndWait(new AssignTrackingIdCommand(hid, trackingIdDto.getTrackingId()));
+    return ResponseEntity.ok().build();
   }
 
   @GetMapping("/me")
   public ResponseEntity<?> me(@RequestHeader(value = "hedvig.token", required = false) Long hid) {
     Optional<MemberEntity> m = repo.findById(hid);
 
-    MemberEntity me =
-        m.orElseGet(
-            () -> {
-              MemberEntity m2 = new MemberEntity();
-              m2.setFirstName("");
-              m2.setLastName("");
-              m2.setBirthDate(LocalDate.now());
-              m2.setStreet("");
-              m2.setCity("");
-              m2.setApartment("");
-              m2.setStatus(null);
-              m2.setSsn("");
-              m2.setEmail("");
-              m2.setCashbackId(cashbackService.getDefaultId().toString());
-              return m2;
-            });
+    MemberEntity me = m.orElseGet(() -> {
+      MemberEntity m2 = new MemberEntity();
+      m2.setFirstName("");
+      m2.setLastName("");
+      m2.setBirthDate(LocalDate.now());
+      m2.setStreet("");
+      m2.setCity("");
+      m2.setApartment("");
+      m2.setStatus(null);
+      m2.setSsn("");
+      m2.setEmail("");
+      m2.setCashbackId(cashbackService.getDefaultId().toString());
+      return m2;
+    });
 
     CashbackOption cashbackOption = null;
 
     if (me.getCashbackId() != null) {
-      cashbackOption =
-          cashbackService
-              .getCashbackOption(UUID.fromString(me.getCashbackId()))
-              .orElseGet(cashbackService::getDefaultCashback);
+      cashbackOption = cashbackService.getCashbackOption(UUID.fromString(me.getCashbackId()))
+          .orElseGet(cashbackService::getDefaultCashback);
     }
 
     InsuranceStatusDTO insuranceStatus = this.productApi.getInsuranceStatus(hid);
     Optional<TrackingIdEntity> tId = trackingRepo.findByMemberId(hid);
 
-    Profile p =
-        new Profile(
-            me.getId().toString(),
-            me.getSsn(),
-            String.format("%s %s", me.getFirstName(), me.getLastName()),
-            me.getFirstName(),
-            me.getLastName(),
-            new ArrayList<>(),
-            me.getBirthDate() == null ? null : me.getBirthDate().until(LocalDate.now()).getYears(),
-            me.getEmail(),
-            me.getStreet(),
-            0,
-            insuranceStatus.getInsuranceStatus().equals("ACTIVE")
-                ? "Betalas via autogiro"
-                : "Betalning sätts upp när försäkringen aktiveras", // ""XXXX XXXX 1234",
-            cashbackOption == null ? null : cashbackOption.name,
-            insuranceStatus.getInsuranceStatus(),
-            insuranceStatus.getInsuranceStatus().equals("ACTIVE")
-                ? LocalDate.now().withDayOfMonth(25)
-                : null,
-            cashbackOption == null ? null : cashbackOption.signature,
-            cashbackOption == null ? null : String.format(cashbackOption.paragraph, me.getFirstName()),
-            cashbackOption == null ? null : cashbackOption.selectedUrl,
-            insuranceStatus.getSafetyIncreasers(),
-            tId.map(TrackingIdEntity::getTrackingId).orElse(null));
+    Profile p = new Profile(me.getId().toString(), me.getSsn(),
+        String.format("%s %s", me.getFirstName(), me.getLastName()), me.getFirstName(), me.getLastName(),
+        new ArrayList<>(), me.getBirthDate() == null ? null : me.getBirthDate().until(LocalDate.now()).getYears(),
+        me.getEmail(), me.getStreet(), 0,
+        insuranceStatus.getInsuranceStatus().equals("ACTIVE") ? "Betalas via autogiro"
+            : "Betalning sätts upp när försäkringen aktiveras", // ""XXXX XXXX 1234",
+        cashbackOption == null ? null : cashbackOption.name, insuranceStatus.getInsuranceStatus(),
+        insuranceStatus.getInsuranceStatus().equals("ACTIVE") ? LocalDate.now().withDayOfMonth(25) : null,
+        cashbackOption == null ? null : cashbackOption.signature,
+        cashbackOption == null ? null : String.format(cashbackOption.paragraph, me.getFirstName()),
+        cashbackOption == null ? null : cashbackOption.selectedUrl, insuranceStatus.getSafetyIncreasers(),
+        tId.map(TrackingIdEntity::getTrackingId).orElse(null));
 
     return ResponseEntity.ok(p);
   }
-  
 }
---
+++ member-service/src/main/java/com/hedvig/memberservice/web/dto/TrackingIdDto.java
@@ -0,0 +1,10 @@
+package com.hedvig.memberservice.web.dto;
+
+import java.util.UUID;
+
+import lombok.Value;
+
+@Value
+public class TrackingIdDto {
+  UUID trackingId;
+}
