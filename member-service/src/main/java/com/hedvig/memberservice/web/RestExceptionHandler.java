a96d41b33f2eae954f1264549eb81e27c4176a7b
Johan Tjelldén <johan@hedvig.com>
2018-11-14 12:58:22 +0100

    Make the RestExceptionHandler lowest precendence

--- member-service/src/main/java/com/hedvig/memberservice/web/RestExceptionHandler.java
+++ member-service/src/main/java/com/hedvig/memberservice/web/RestExceptionHandler.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-@Order(Ordered.HIGHEST_PRECEDENCE)
+@Order(Ordered.LOWEST_PRECEDENCE)
 @ControllerAdvice
 public class RestExceptionHandler extends ResponseEntityExceptionHandler {
 
