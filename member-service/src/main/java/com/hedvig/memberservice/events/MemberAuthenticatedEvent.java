80ce2b8797ecd1033db3fba1522251bec1740ad6
Johan Tjelldén <johan@hedvig.com>
2019-01-09 10:35:28 +0100

    Make memberAuthenticatedEvents tracable

--- member-service/src/main/java/com/hedvig/memberservice/events/MemberAuthenticatedEvent.java
+++ member-service/src/main/java/com/hedvig/memberservice/events/MemberAuthenticatedEvent.java
@@ -2,8 +2,18 @@ package com.hedvig.memberservice.events;
 
 import lombok.Value;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Value
-public class MemberAuthenticatedEvent {
+public class MemberAuthenticatedEvent implements Traceable{
   private Long memberId;
   private String bankIdReferenceToken;
+
+  @Override
+  public Map<String, Object> getValues() {
+    Map result = new HashMap();
+    result.put("Member authenticated", "");
+    return result;
+  }
 }
