0428b4eb16d065aee6e2dd62aaa9ff08c25a3445
AlexSJ1990 <alex.sinclair.joice@gmail.com>
2019-10-14 15:31:51 +0200

    return a 422 (unprocessable entity) rather than a 402 and add error code in errorResponseDto

--- member-service/src/main/java/com/hedvig/memberservice/services/member/dto/ErrorResponseDto.kt
+++ member-service/src/main/java/com/hedvig/memberservice/services/member/dto/ErrorResponseDto.kt
@@ -1,5 +1,10 @@
 package com.hedvig.memberservice.services.member.dto
 
 data class ErrorResponseDto (
+    val errorCode: ErrorCodes,
     val errorMessage: String
 )
+
+enum class ErrorCodes {
+    MEMBER_HAS_EXISTING_INSURANCE
+}
--- member-service/src/main/java/com/hedvig/memberservice/web/v2/SignController.java
+++ member-service/src/main/java/com/hedvig/memberservice/web/v2/SignController.java
@@ -1,6 +1,7 @@
 package com.hedvig.memberservice.web.v2;
 
 import com.hedvig.memberservice.services.SigningService;
+import com.hedvig.memberservice.services.member.dto.ErrorCodes;
 import com.hedvig.memberservice.services.member.dto.ErrorResponseDto;
 import com.hedvig.memberservice.web.v2.dto.*;
 import lombok.val;
@@ -42,7 +43,7 @@ public class SignController {
       val result = signingService.signUnderwriterQuote(memberId, underwriterQuoteSignRequest);
       return ResponseEntity.ok(new UnderwriterQuoteSignResponse(result.getSignId(),result.getMemberIsSigned()));
     } catch(Exception exception) {
-      return ResponseEntity.status(402).body(new ErrorResponseDto("Member already has an existing product"));
+      return ResponseEntity.status(422).body(new ErrorResponseDto(ErrorCodes.MEMBER_HAS_EXISTING_INSURANCE, "Not able to sign quote, the social security given is already associated with a signed product"));
     }
   }
 
@@ -55,5 +56,4 @@ public class SignController {
         .map(x -> ResponseEntity.ok(SignStatusResponse.CreateFromEntity(x)))
         .orElseGet(() -> ResponseEntity.notFound().build());
   }
-
 }
