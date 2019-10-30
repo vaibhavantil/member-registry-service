94577c6eccfc425ba0dff2fc0dff2009258123de
vonElfvin <elvin.granat@gmail.com>
2019-10-30 16:24:04 +0100

    Add profiles to customer.io cleanup

--- member-service/src/main/java/com/hedvig/integration/customerIO/CustomerIO.kt
+++ member-service/src/main/java/com/hedvig/integration/customerIO/CustomerIO.kt
@@ -2,8 +2,10 @@ package com.hedvig.integration.customerIO
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.cloud.openfeign.EnableFeignClients
+import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 
+@Profile("customer.io")
 @ConditionalOnProperty(value = ["customerio.siteId", "customerio.apiKey"], matchIfMissing = false)
 @Component
 @EnableFeignClients
