15785865eedab8749fa8e517b4e48f77a2e76242
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-01-07 13:09:41 +0100

    pr fixes

--- member-service/src/main/java/com/hedvig/config/RestTemplateConfig.java
+++ member-service/src/main/java/com/hedvig/config/RestTemplateConfig.java
@@ -1,6 +1,5 @@
 package com.hedvig.config;
 
-import com.hedvig.memberservice.CustomClientHttpRequestInterceptor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,15 +15,9 @@ public class RestTemplateConfig {
 
     @Bean
     public org.springframework.web.client.RestTemplate restTemplate(
-            RestTemplateBuilder builder,
-            CustomClientHttpRequestInterceptor customClientHttpRequestInterceptor) {
-        org.springframework.web.client.RestTemplate restTemplate =
-                new org.springframework.web.client.RestTemplate(
-                        new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
-        List<ClientHttpRequestInterceptor> interceptors =
-                Collections.singletonList(customClientHttpRequestInterceptor);
-        restTemplate.setInterceptors(interceptors);
-        return restTemplate;
+            RestTemplateBuilder builder) {
+      return new org.springframework.web.client.RestTemplate(
+              new BufferingClientHttpRequestFactory(new SimpleClientHttpRequestFactory()));
     }
 
 }
