791269fdca1912e9263a6e197e7111f45e5f4f20
Johan Tjelldén <johan@hedvig.com>
2020-02-14 15:55:11 +0100

    Add some logging on weird autostart tokens

--- member-service/src/main/java/com/hedvig/memberservice/web/v2/AuthControllerV2.kt
+++ member-service/src/main/java/com/hedvig/memberservice/web/v2/AuthControllerV2.kt
@@ -3,7 +3,8 @@ package com.hedvig.memberservice.web.v2
 import com.hedvig.memberservice.services.v2.BankIdServiceV2
 import com.hedvig.memberservice.util.getEndUserIp
 import com.hedvig.memberservice.web.v2.dto.AuthResponse
-import org.axonframework.commandhandling.gateway.CommandGateway
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.slf4j.MDC
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
@@ -14,9 +15,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/v2/member/bankid/")
 class AuthControllerV2(
-        private val commandGateway: CommandGateway,
-        private val bankIdService: BankIdServiceV2
+    private val bankIdService: BankIdServiceV2
 ) {
+    private val log: Logger = LoggerFactory.getLogger(AuthControllerV2::class.java)
+
     @PostMapping("auth")
     fun auth(@RequestHeader("hedvig.token") memberId: Long, @RequestHeader(value = "x-forwarded-for", required = false) forwardedIp: String?): ResponseEntity<AuthResponse> {
         MDC.put("memberId", memberId.toString())
@@ -25,6 +27,11 @@ class AuthControllerV2(
             .getEndUserIp("Header 'x-forwarded-for' was not included when calling AuthControllerV2 auth! MemberId:$memberId")
 
         val status = bankIdService.auth(memberId, endUserIp)
+
+        if(status.autoStartToken == null) {
+            log.error("AutoStartToken is null for orderRef {}", status.orderRef)
+        }
+
         return ResponseEntity.ok(AuthResponse(status.autoStartToken))
     }
 }
