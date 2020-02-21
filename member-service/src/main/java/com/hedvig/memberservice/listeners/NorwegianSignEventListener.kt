66b1d91d324465847f0683f346a7b318902ea536
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-02-21 07:23:42 +0100

    sign event listener

--- member-service/src/main/java/com/hedvig/memberservice/listeners/NorwegianSignEventListener.kt
+++ member-service/src/main/java/com/hedvig/memberservice/listeners/NorwegianSignEventListener.kt
@@ -1,18 +1,16 @@
 package com.hedvig.memberservice.listeners
 
-import com.hedvig.external.event.NorwegianAuthenticationEvent
+import com.hedvig.external.event.NorwegianSignEvent
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
-class NorwegianAuthEventListener {
+class NorwegianSignEventListener {
 
     @Async
     @EventListener
-    fun handleNorwegianAuthEvent(norwegianAuthEvent: NorwegianAuthenticationEvent) {
-        //TODO: member is auth publish to redis!
+    fun handleNorwegianSignEvent(norwegianSignEvent: NorwegianSignEvent) {
+        //TODO: member is sign publish to redis!
     }
 }
-
-
