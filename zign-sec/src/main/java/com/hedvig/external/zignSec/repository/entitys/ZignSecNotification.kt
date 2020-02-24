0a6056bf6185355d80fe5f0241f832dfee801d24
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-02-24 10:49:49 +0100

    this one can be large

--- zign-sec/src/main/java/com/hedvig/external/zignSec/repository/entitys/ZignSecNotification.kt
+++ zign-sec/src/main/java/com/hedvig/external/zignSec/repository/entitys/ZignSecNotification.kt
@@ -1,6 +1,7 @@
 package com.hedvig.external.zignSec.repository.entitys
 
 import com.hedvig.external.zignSec.client.dto.ZignSecNotificationRequest
+import javax.persistence.Column
 import javax.persistence.ElementCollection
 import javax.persistence.Embeddable
 import javax.persistence.OneToMany
@@ -11,6 +12,7 @@ class ZignSecNotification(
     val errors: List<Error>,
     val identity: Identity?,
     val method: String?,
+    @Column(columnDefinition="VARCHAR(10000)")
     val bankIdNoOidc: String?
 ) {
     constructor() : this(
