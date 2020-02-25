688d3fca03a4959a43d82939e02189e16cdd1d78
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-02-25 10:08:38 +0100

    reasign member

--- member-service/src/main/java/com/hedvig/memberservice/listeners/NorwegianAuthEventListener.kt
+++ member-service/src/main/java/com/hedvig/memberservice/listeners/NorwegianAuthEventListener.kt
@@ -1,27 +1,20 @@
 package com.hedvig.memberservice.listeners
 
-import com.hedvig.external.authentication.dto.NorwegianAuthenticationResult
 import com.hedvig.external.event.NorwegianAuthenticationEvent
-import com.hedvig.memberservice.services.redispublisher.AuthSessionUpdatedEventStatus
-import com.hedvig.memberservice.services.redispublisher.RedisEventPublisher
+import com.hedvig.memberservice.services.NorwegianBankIdService
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 
 @Component
 class NorwegianAuthEventListener(
-    private val redisEventPublisher: RedisEventPublisher
+    private val norwegianBankIdService: NorwegianBankIdService
 ) {
 
     @Async
     @EventListener
     fun handleNorwegianAuthEvent(norwegianAuthEvent: NorwegianAuthenticationEvent) {
-        when (val result = norwegianAuthEvent.message) {
-            is NorwegianAuthenticationResult.Completed ->
-                redisEventPublisher.onAuthSessionUpdated(result.memberId, AuthSessionUpdatedEventStatus.SUCCESS)
-            is NorwegianAuthenticationResult.Failed ->
-                redisEventPublisher.onAuthSessionUpdated(result.memberId, AuthSessionUpdatedEventStatus.FAILED)
-        }
+        norwegianBankIdService.finishedAuthentication(norwegianAuthEvent.message)
     }
 }
 
--- member-service/src/main/java/com/hedvig/memberservice/services/NorwegianBankIdService.kt
+++ member-service/src/main/java/com/hedvig/memberservice/services/NorwegianBankIdService.kt
@@ -1,12 +1,23 @@
 package com.hedvig.memberservice.services
 
 import com.hedvig.external.authentication.NorwegianAuthentication
+import com.hedvig.external.authentication.dto.NorwegianAuthenticationResult
 import com.hedvig.external.authentication.dto.NorwegianBankIdAuthenticationRequest
+import com.hedvig.integration.apigateway.ApiGatewayService
+import com.hedvig.memberservice.commands.InactivateMemberCommand
+import com.hedvig.memberservice.query.SignedMemberRepository
+import com.hedvig.memberservice.services.redispublisher.AuthSessionUpdatedEventStatus
+import com.hedvig.memberservice.services.redispublisher.RedisEventPublisher
+import org.axonframework.commandhandling.gateway.CommandGateway
 import org.springframework.stereotype.Service
 
 @Service
 class NorwegianBankIdService(
-    private val norwegianAuthentication: NorwegianAuthentication
+    private val norwegianAuthentication: NorwegianAuthentication,
+    private val commandGateway: CommandGateway,
+    private val redisEventPublisher: RedisEventPublisher,
+    private val signedMemberRepository: SignedMemberRepository,
+    private val apiGatewayService: ApiGatewayService
 ) {
     fun authenticate(request: NorwegianBankIdAuthenticationRequest) =
         norwegianAuthentication.auth(
@@ -21,6 +32,27 @@ class NorwegianBankIdService(
                 acceptLanguage?.toTwoLetterLanguage() ?: "NO"
             )
         )
+
+    fun finishedAuthentication(result: NorwegianAuthenticationResult) {
+        when (result) {
+            is NorwegianAuthenticationResult.Completed -> {
+                val signedMember = signedMemberRepository.findBySsn(result.ssn)
+                if (signedMember.isPresent) {
+                    if (result.memberId != signedMember.get().id) {
+                        commandGateway.sendAndWait<Any>(InactivateMemberCommand(result.memberId))
+                        apiGatewayService.reassignMember(result.memberId, signedMember.get().id)
+                    }
+                    redisEventPublisher.onAuthSessionUpdated(result.memberId, AuthSessionUpdatedEventStatus.SUCCESS)
+                } else {
+                    redisEventPublisher.onAuthSessionUpdated(result.memberId, AuthSessionUpdatedEventStatus.FAILED)
+                }
+
+                redisEventPublisher.onAuthSessionUpdated(result.memberId, AuthSessionUpdatedEventStatus.SUCCESS)
+            }
+            is NorwegianAuthenticationResult.Failed ->
+                redisEventPublisher.onAuthSessionUpdated(result.memberId, AuthSessionUpdatedEventStatus.FAILED)
+        }
+    }
 
     fun getStatus(memberId: Long) = norwegianAuthentication.getStatus(memberId)
 
