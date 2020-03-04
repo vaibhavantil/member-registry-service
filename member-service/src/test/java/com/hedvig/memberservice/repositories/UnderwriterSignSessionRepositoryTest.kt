ad141d930491196c7a9d3476ddb505109ce5b13f
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-03-04 14:30:31 +0100

    storeUnderwriterSignSessionWithSingReference

---
+++ member-service/src/test/java/com/hedvig/memberservice/repositories/UnderwriterSignSessionRepositoryTest.kt
@@ -0,0 +1,16 @@
+package com.hedvig.memberservice.repositories
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@DataJpaTest
+@RunWith(SpringRunner::class)
+class UnderwriterSignSessionRepositoryTest {
+
+    @Test
+    fun storeUnderwriterSignSessionWithSingReference() {
+
+    }
+}
