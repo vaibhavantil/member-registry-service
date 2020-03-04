998fef77824aa2e56b22b1b369596529d17afc66
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-03-04 16:30:19 +0100

    better startSwedishBankIdSignSession

--- member-service/src/test/java/com/hedvig/memberservice/services/UnderwriterSigningServiceImplTest.kt
+++ member-service/src/test/java/com/hedvig/memberservice/services/UnderwriterSigningServiceImplTest.kt
@@ -1,13 +1,31 @@
 package com.hedvig.memberservice.services
 
+import com.hedvig.external.bankID.bankIdTypes.OrderResponse
+import com.hedvig.memberservice.entities.UnderwriterSignSessionEntity
+import com.hedvig.memberservice.query.UnderwriterSignSessionRepository
+import com.hedvig.memberservice.services.member.dto.StartSwedishSignResponse
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
 import org.mockito.junit.MockitoJUnitRunner
+import java.util.*
 import org.mockito.Mockito.`when` as whenever
 
 @RunWith(MockitoJUnitRunner::class)
 class UnderwriterSigningServiceImplTest {
+
+    @Mock
+    lateinit var underwriterSignSessionRepository: UnderwriterSignSessionRepository
+
+    @Mock
+    lateinit var swedishBankIdSigningService: SwedishBankIdSigningService
+
+    @Captor
+    lateinit var captor: ArgumentCaptor<UnderwriterSignSessionEntity>
 
     private lateinit var sut: UnderwriterSigningService
 
@@ -18,7 +36,29 @@ class UnderwriterSigningServiceImplTest {
 
     @Test
     fun startSwedishBankIdSignSession() {
+        whenever(swedishBankIdSigningService.startSign(memberId, swedishSSN, ip, false))
+            .thenReturn(StartSwedishSignResponse(
+                signId = 0,
+                bankIdOrderResponse = OrderResponse(
+                    swedishOrderRef,
+                    autoStartToken
+                )
+            ))
 
-        sut.startSwedishBankIdSignSession()
+        whenever(underwriterSignSessionRepository.save(captor.capture()))
+
+        val response = sut.startSwedishBankIdSignSession(memberId, swedishSSN, ip, false)
+
+        assertThat(response.autoStartToken).isEqualTo(autoStartToken)
+        assertThat(captor.value.signReference).isEqualTo(swedishOrderRef)
+    }
+
+    companion object {
+        private val underwriterSessionRef = UUID.randomUUID()
+        private const val swedishOrderRef = "orderRef"
+        private const val autoStartToken = "autoStartToken"
+        private const val memberId = 1337L
+        private const val swedishSSN = "1912121212"
+        private const val ip = "1.0.0.0"
     }
 }
