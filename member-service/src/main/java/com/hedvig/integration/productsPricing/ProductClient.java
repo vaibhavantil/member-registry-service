58af448532ffc2a53b84853d1d22dce285a454c3
vonElfvin <elvin.granat@gmail.com>
2020-10-23 10:52:11 +0200

    Clean up deprecated usage of products

--- member-service/src/main/java/com/hedvig/integration/productsPricing/ProductClient.java
+++ member-service/src/main/java/com/hedvig/integration/productsPricing/ProductClient.java
@@ -13,34 +13,10 @@ import javax.validation.Valid;
 public interface ProductClient {
 
   @RequestMapping(value = "/i/campaign/member/create", method = RequestMethod.POST)
-  ResponseEntity<Void> memberCreated(@RequestBody MemberCreatedRequest req);
-
-  @RequestMapping(value = "/_/insurance/contractSigned", method = RequestMethod.POST)
-  String contractSinged(@RequestBody ContractSignedRequest req);
+  ResponseEntity<Void> createdCampaignMember(@RequestBody MemberCreatedRequest req);
 
   @RequestMapping(value = "/i/campaign/member/update/name", method = RequestMethod.POST)
-  ResponseEntity<Void> memberNameUpdate(@RequestBody MemberNameUpdateRequest req);
-
-  @RequestMapping(value = "/_/insurance/{memberId}/safetyIncreasers", method = RequestMethod.GET)
-  ResponseEntity<SafetyIncreasersDTO> getSafetyIncreasers(@PathVariable("memberId") long memberId);
-
-  @RequestMapping(value = "/insurance/{memberId}/insuranceStatus")
-  ResponseEntity<InsuranceStatusDTO> getInsuranceStatus(@PathVariable("memberId") long memberId);
-
-  @RequestMapping(value = "/_/insurance/contract/{memberId}")
-  ResponseEntity<byte[]> getContract(@PathVariable("memberId") String contractId);
-
-  @RequestMapping(value = "/_/insurance/{memberId}/setCancellationDate")
-  ResponseEntity<String> setCancellationDate(
-      @PathVariable("memberId") Long memberId, SetCancellationDateRequest body);
-
-  @GetMapping("/_/insurance/searchByActivationDate?activationDate={date}")
-  ResponseEntity<List<InsuranceNotificationDTO>> getInsurancesByActivationDate(
-      @PathVariable("date") String activationDate);
-
-  @GetMapping("/_/insurance/{memberId}/hasProductToSign")
-  ResponseEntity<ProductToSignStatusDTO> hasProductToSign(
-      @PathVariable("memberId") String memberId);
+  ResponseEntity<Void> updateCampaignMemberName(@RequestBody MemberNameUpdateRequest req);
 
   @GetMapping("/_/contracts/members/{memberId}/hasContract")
   ResponseEntity<Boolean> hasContract(@PathVariable("memberId") Long memberId);
