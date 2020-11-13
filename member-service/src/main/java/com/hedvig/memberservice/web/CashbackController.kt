062f1137c027f25c0a27a71797636c9a28d97a9d
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2020-11-13 15:32:26 +0100

    locale header override

--- member-service/src/main/java/com/hedvig/memberservice/web/CashbackController.kt
+++ member-service/src/main/java/com/hedvig/memberservice/web/CashbackController.kt
@@ -1,8 +1,11 @@
 package com.hedvig.memberservice.web
 
+import com.hedvig.memberservice.aggregates.PickedLocale
+import com.hedvig.memberservice.aggregates.toPickedLocale
 import com.hedvig.memberservice.services.cashback.CashbackService
 import com.hedvig.memberservice.services.exceptions.MemberNotFoundException
 import com.hedvig.memberservice.web.dto.CashbackOption
+import com.hedvig.resolver.LocaleResolver
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,11 +18,13 @@ class CashbackController @Autowired constructor(
 ) {
     @GetMapping("options")
     fun options(
-        @RequestHeader(value = "hedvig.token") memberId: Long
+        @RequestHeader(value = "hedvig.token") memberId: Long,
+        @RequestHeader(value = "Locale", required = false) locale: String?,
+        @RequestHeader(value = "Accept-Language", required = false) acceptLanguage: String?
     ): ResponseEntity<List<CashbackOption>> {
         return try {
             val cashbackOptions: List<CashbackOption> = cashbackService
-                .getOptions(memberId)
+                .getOptions(memberId, resolveLocaleFromHeaders(locale, acceptLanguage))
             ResponseEntity.ok(cashbackOptions)
         } catch (e: MemberNotFoundException) {
             ResponseEntity.notFound().build()
@@ -28,15 +33,31 @@ class CashbackController @Autowired constructor(
 
     @PostMapping("")
     fun cashback(
-        @RequestHeader(value = "hedvig.token") hid: Long, @RequestParam optionId: UUID?
+        @RequestHeader(value = "hedvig.token") hid: Long, @RequestParam optionId: UUID?,
+        @RequestHeader(value = "Locale", required = false) locale: String?,
+        @RequestHeader(value = "Accept-Language", required = false) acceptLanguage: String?
     ): ResponseEntity<String> {
         return try {
             optionId?.let {
-                cashbackService.selectCashbackOption(hid, it)
+                cashbackService.selectCashbackOption(hid, it, resolveLocaleFromHeaders(locale, acceptLanguage))
                 return ResponseEntity.noContent().build()
             } ?: ResponseEntity.notFound().build()
         } catch (e: MemberNotFoundException) {
             ResponseEntity.notFound().build()
         }
+    }
+
+    private fun resolveLocaleFromHeaders(locale: String?, acceptLanguage: String?): PickedLocale? {
+        locale?.let {
+            try {
+                return PickedLocale.valueOf(it)
+            } catch (e: Exception) {
+                // no-op
+            }
+        }
+
+        return LocaleResolver
+            .resolveNullableLocale(acceptLanguage)
+            ?.toPickedLocale()
     }
 }
