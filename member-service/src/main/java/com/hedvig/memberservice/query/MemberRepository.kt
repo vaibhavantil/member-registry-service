98f6240c496814a492866942bef54f49c77eecf3
vonElfvin <elvin.granat@gmail.com>
2020-12-14 16:37:41 +0100

    Cleanup customer.io from Norwegian, Danish and Rapio members (also SimpleSign as well)

--- member-service/src/main/java/com/hedvig/memberservice/query/MemberRepository.kt
+++ member-service/src/main/java/com/hedvig/memberservice/query/MemberRepository.kt
@@ -6,7 +6,9 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import org.springframework.stereotype.Repository
 
+@Repository
 interface MemberRepository : JpaRepository<MemberEntity, Long> {
 
     fun findBySsn(ssn: String): List<MemberEntity>
