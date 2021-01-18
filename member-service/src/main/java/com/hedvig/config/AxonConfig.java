bdef59992f87b7ab26210ac5f351401cf6f7693c
Fredrik Areschoug <fredrik.areschoug@gmail.com>
2021-01-18 14:25:51 +0100

    removed backfill code

--- member-service/src/main/java/com/hedvig/config/AxonConfig.java
+++ member-service/src/main/java/com/hedvig/config/AxonConfig.java
@@ -72,12 +72,6 @@ public class AxonConfig {
           x ->
               TrackingEventProcessorConfiguration.forSingleThreadedProcessing()
                   .andInitialTrackingToken(StreamableMessageSource::createHeadToken));
-
-      config.registerTrackingEventProcessor(
-          "BackfillMemberIdentifiedEventNorway",
-          x ->
-              TrackingEventProcessorConfiguration.forSingleThreadedProcessing()
-                  .andInitialTrackingToken(StreamableMessageSource::createTailToken));
   }
 
 
--- member-service/src/test/java/com/hedvig/memberservice/MemberAggregateTests.kt
+++ member-service/src/test/java/com/hedvig/memberservice/MemberAggregateTests.kt
@@ -253,14 +253,13 @@ class MemberAggregateTests {
             .expectEvents(
                 NewCashbackSelectedEvent(memberId, DEFAULT_CASHBACK.toString()),
                 NorwegianSSNUpdatedEvent(memberId, personalNumber),
-                /** TODO: BackfillMemberIdentifiedEventNorwayListener
                  MemberIdentifiedEvent(
                     memberId,
                     MemberIdentifiedEvent.NationalIdentification(personalNumber, MemberIdentifiedEvent.Nationality.NORWAY),
                     MemberIdentifiedEvent.IdentificationMethod.NORWEGIAN_BANK_ID,
                     "Test",
                     "Testsson"
-                ),*/
+                ),
                 NorwegianMemberSignedEvent(memberId, personalNumber, provideJsonResponse, referenceId)
             )
     }
