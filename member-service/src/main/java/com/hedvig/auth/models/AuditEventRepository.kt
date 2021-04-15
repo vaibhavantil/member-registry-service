be11f7438d6100955394698a94ca61b993df0a83
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-04-15 15:31:54 +0200

    disambiguate AuditEventRepository from existing spring one

--- member-service/src/main/java/com/hedvig/auth/models/AuditEventRepository.kt
+++ member-service/src/main/java/com/hedvig/auth/models/AuditEventRepository.kt
@@ -1,6 +1,9 @@
 package com.hedvig.auth.models
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+// disambiguate from existing auditEventRepository in spring
+@Repository(value = "com.hedvig.auth.models.AuditEventRepository")
 internal interface AuditEventRepository: JpaRepository<AuditEvent, Long> {
 }
