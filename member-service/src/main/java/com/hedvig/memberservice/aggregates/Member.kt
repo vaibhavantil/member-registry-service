efac71aba0f2cbde4e28be6eac651d945580d0fd
Fredrik Tõnisson-Bystam <fredrik.bystam@hedvig.com>
2021-04-21 15:05:57 +0200

    remove unused enum

--- member-service/src/main/java/com/hedvig/memberservice/aggregates/Member.kt
+++ member-service/src/main/java/com/hedvig/memberservice/aggregates/Member.kt
@@ -12,10 +12,5 @@ data class Member(
     val pickedLocale: PickedLocale? = null,
     val livingAddress: LivingAddress? = null,
     val birthDate: LocalDate? = null,
-    val countryCode: String? = null,
-    val identificationSource: IdentificationSource? = null
+    val countryCode: String? = null
 )
-
-enum class IdentificationSource {
-    SE_BANK_ID, NO_BANK_ID, NEM_ID
-}
